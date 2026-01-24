60cb141e0449c9f255b0ebc6a948c4bccf85efd1
soohyun111 <jenna38@naver.com>
2026-01-25 06:13:07 +0900

    Delete components directory

--- components/Banner.tsx
+++
@@ -1,9 +0,0 @@
-import banner from "../img/banner.jpg";
-
-export default function Banner() {
-  return (
-    <div className="banner">
-      <img  src={banner} className="banner-image"/>
-    </div>
-  );
-}
